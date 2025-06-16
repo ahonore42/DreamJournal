0a41de33e2fe9e073dd8cd9c424c0e69b433c6e4
ahonore42 <a1honore80@gmail.com>
2025-06-16 09:02:10 -0700

    feat: add profile to tab layout

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,9 @@
+import ProfileScreen from "@/components/screens/ProfileScreen";
+import StarryBackground from "@/components/layout/StarryBackground";
+export default function TabTwoScreen() {
+  return (
+    <StarryBackground>
+      <ProfileScreen />
+    </StarryBackground>
+  );
+}
