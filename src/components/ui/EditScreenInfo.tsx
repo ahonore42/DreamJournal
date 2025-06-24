11725c39c2b88fe7cd69b618a0a85d4515d0b2cf
ahonore42 <a1honore80@gmail.com>
2025-06-23 23:41:00 -0700

    feat: update text, view components

--- src/components/ui/EditScreenInfo.tsx
+++ src/components/ui/EditScreenInfo.tsx
@@ -3,33 +3,18 @@ import { StyleSheet } from "react-native";
 import { ExternalLink } from "./ExternalLink";
 import { StyledText } from "./StyledText";
 import { Text, View } from "../layout/Themed";
-import { theme } from "@/constants/Colors";
 
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
       <View style={styles.getStartedContainer}>
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)"
-        >
-          Open up the code for this screen:
-        </Text>
+        <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
 
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)"
-        >
+        <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
           <StyledText>{path}</StyledText>
         </View>
 
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)"
-        >
+        <Text style={styles.getStartedText}>
           Change any of the text, save the file, and your app will automatically update.
         </Text>
       </View>
@@ -39,7 +24,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.helpLink}
           href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet"
         >
-          <Text style={styles.helpLinkText} lightColor={theme.text}>
+          <Text style={styles.helpLinkText}>
             Tap here if your app doesn't automatically update after making changes
           </Text>
         </ExternalLink>
